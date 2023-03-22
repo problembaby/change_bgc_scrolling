293620a0a41084cf77ebbcc04b66ddfc32d8c994
ocy <cjdduf3@naver.com>
2023-03-22 09:42:25 +0900

    chang_bgc_scrolling create a folder

--- change_bgc_scrolling/scrolling.js
+++ change_bgc_scrolling/scrolling.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
   let wHeight = $(window).height(); //윈도우 높이
- // console.log("창높이 : ",wHeight);
+  console.log("창높이 : ",wHeight);
 
   $('.slide').height(wHeight)
   .scrollie({
     scrollOffset : -50, // 상단에서 50px 떨어졌을시 작동
     scrollingInView: function(elem){
       let bgColor = elem.data('background');
-      //console.log("색상 : ",bgColor);
-
+      console.log("색상 : ",bgColor);
       $('body').css('background-color',bgColor) 
 
     }
